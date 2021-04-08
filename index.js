164f2c5de6dca9a7676fa1d639025e226a455b05
musiccity365 <m_sackstein@yahoo.com>
2021-04-07 20:23:11 -0400

    commit tree e578330cb529d4eb002c79fa7cd520216ba8cb1d

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards( namesArray, event ) {
+  let thankYouCards = []
+  for ( let i = 0; i < namesArray.length; i++ ) {
+    thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+  }
+  return thankYouCards
+}
+
+function countDown( startingNumber ) {
+  while ( startingNumber > 0 ) {
+    console.log( startingNumber );
+    startingNumber -= 1;
+  }
+  console.log( startingNumber );
+}
